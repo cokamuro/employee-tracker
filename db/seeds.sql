61c3eaf0bea83df828fe3fb0c402655cf6ff0ed1
Chris Okamuro <cokamuro@gmail.com>
2022-06-26 10:48:22 -0400

    switched prompts from multiple arrays to a single array using when clauses.  trying to figure our the promise chaining/async

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+DELETE FROM employee;
+DELETE FROM role;
+DELETE FROM department;
+
+INSERT INTO department (id,name)
+VALUES (1,"R&D"),
+       (2,"Admin"),
+       (3,"HR");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Software Engineer L4", 90000,1),
+        (2, "Software Engineer L5", 120000,1),
+        (3, "Software Engineer L6", 15000,1),
+        (4, "Software Eng. Manager L7", 220000,1),
+        (5, "CEO", 750000,2),
+        (6, "CFO", 350000,2),
+        (7, "CTO", 400000,2),
+        (8, "HR Director", 750000,3),
+        (9, "Benefits Coordinator", 350000,3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4,"Chris", "Okamuro", 7,4),
+        (1,"Sela", "Okamuro", 4,4),
+        (2,"Aiko", "Okamuro", 1,1),
+        (3,"Skoshi", "Okamuro", 2,1);
+        
+
+
+       
