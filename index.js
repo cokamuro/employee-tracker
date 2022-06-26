221650177dcdb7d109e79a9a48ab591f10ec40da
Chris Okamuro <cokamuro@gmail.com>
2022-06-26 16:26:39 -0400

    completed commenting and cleanup of index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const mysql = require('mysql2');
 const { prompt } = require('inquirer');
 const prompts = require('./lib/prompts.js');
 const functions = require('./lib/functions.js');
-const { async } = require('rxjs');
-const { arrayBuffer } = require('stream/consumers');
 
 const db = mysql.createConnection(
     {
@@ -12,76 +10,86 @@ const db = mysql.createConnection(
         password: '',
         database: 'employeetracker'
     },
-    console.log(`Connected to the employeetracker database.`)
 );
 
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-
 const init = () => {
+    //program starts here
     showMenu()
 };
 
 const prepMenu = async () => {
     //read departments 
-    const arrDept= await functions.returnDepartmentArray(db) 
-    //set department list into the choices for department on role question
-    prompts.menu[4].choices=arrDept;
+    const arrDept = await functions.returnDepartmentArray(db);
+    //set department list into the choices for questions with department selection
+    prompts.menu[4].choices = arrDept;
+    prompts.menu[12].choices = arrDept;
+    prompts.menu[13].choices = arrDept;
     //read roles
-    const arrRole= await functions.returnRoleArray(db) 
-    //set role list into the choices for department on employee question
-    prompts.menu[7].choices=arrRole;
+    const arrRole = await functions.returnRoleArray(db);
+    //set role list into the choices for questions with role selection
+    prompts.menu[7].choices = arrRole;
+    prompts.menu[10].choices = arrRole;
+    prompts.menu[14].choices = arrRole;
     //read list of employees
-    const arrEmp= await functions.returnEmployeeArray(db) 
-    //add option for no manager
-    arrEmp.unshift({name: "(none)", value: null})
-    //set manager list into the choices for department on employee question
-    prompts.menu[8].choices=arrEmp;
+    const arrEmp = await functions.returnEmployeeArray(db);
+    let arrMgr = JSON.parse(JSON.stringify(arrEmp));
+    arrMgr.unshift({ name: "(none)", value: null });
+    //set manager list into the choices for questions with manager selection that can also be null
+    prompts.menu[8].choices = arrMgr;
+    prompts.menu[17].choices = arrMgr;
+    //set employee list into the choices for questions with employee selection
+    prompts.menu[9].choices = arrEmp;
+    prompts.menu[15].choices = arrEmp;
+    prompts.menu[16].choices = arrEmp;
+    //read list of managers
+    const arrManagers = await functions.returnManagerArray(db);
+    //set manager list for question with manager selection
+    prompts.menu[11].choices = arrManagers;
 }
 
 const showMenu = async () => {
+    //call function to populate prompt choices
     await prepMenu()
+    //present user with menu selections
     const data = await prompt(prompts.menu);
-    console.log()
     if (data.menuSelection != -1) {
+        //call menu handler function
         menuHandler(data);
-        //showMenu();
+        //re-display menu after last function completes
+        showMenu();
     } else {
+        //exit program
         console.log("Exiting program");
         process.exit();
     }
 }
 
 const menuHandler = (data) => {
-    const menuSelection=data.menuSelection;
+    const menuSelection = data.menuSelection;
     if (menuSelection < 10) {
-        functions.queries(db, menuSelection);
-    } else if (menuSelection < 20) {
-        console.log(data)
+        //execute all queries using functions.queries
+        functions.queries(db, data);
+    } else if (menuSelection >= 10 && menuSelection < 20) {
         //add a department
-        if (menuSelection == 10) {functions.insertDepartment(db, data.departmentName)};
+        if (menuSelection == 10) { functions.insertDepartment(db, data.departmentName) };
         //add a role
-        if (menuSelection == 11) {functions.insertRole(db, data.roleTitle, data.roleSalary, data.roleDepartment)};
+        if (menuSelection == 11) { functions.insertRole(db, data.roleTitle, data.roleSalary, data.roleDepartment) };
         //add an employee            
-        if (menuSelection == 12) {functions.insertEmployee(db, data.employeeFirst_name, data.employeeLast_name, data.employeeRole, data.employeeManager)};
-    } else {
-        functions.updates(db, menuSelection);
-    }
+        if (menuSelection == 12) { functions.insertEmployee(db, data.employeeFirst_name, data.employeeLast_name, data.employeeRole, data.employeeManager) };
+    } else if (menuSelection >= 20 && menuSelection < 30) {
+        //update an employee's role
+        if (menuSelection == 20) { functions.updateEmployeeRoleId(db, data.employeeToUpdate, data.employeeRole) };
+        //update an employee's manager
+        if (menuSelection == 21) { functions.updateEmployeeManagerId(db, data.employeeToUpdate, data.selectedManager) };
+    } else if (menuSelection >= 30 && menuSelection < 40) {
+        //execute all deletes using functions.deletes
+        //delete a department
+        if (menuSelection == 30) { functions.deletes(db, menuSelection, data.selectedDepartment) };
+        //delete a role
+        if (menuSelection == 31) { functions.deletes(db, menuSelection, data.selectedRole) };
+        //delete an employee
+        if (menuSelection == 31) { functions.deletes(db, menuSelection, data.selectedEmployee) };
+    };
 };
 
 init();
